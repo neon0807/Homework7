6ffffa7317c4b4f430d9741e2c35c5cbfccc488f
neon0807 <vitaliivorob3@gmail.com>
2022-12-13 20:35:34 +0300

    Проверьте ДЗ

---
+++ src/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+    private String name;
+    private int publishingYear;
+
+    public Book(String name, Author author, int publishingYear) {
+        this.name = name;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Author author1 = new Author("Стивен", "Кинг");
+        Book book1 = new Book("Оно", author1,  1986);
+        Author author2 = new Author("Дмитрий", "Глуховский");
+        Book book2 = new Book("Метро 2033", author2, 2005);
+//      System.out.println(author1.getFirstName() + " " + author1.getLastName() + " " + book1.getName() + " " + book1.getPublishingYear() + "г.");
+//        System.out.println(author2.getFirstName() + " " + author2.getLastName() + " " + book2.getName() + " " + book2.getPublishingYear() + "г.");
+        book1.setPublishingYear(2000);
+//        System.out.println(author1.getFirstName() + " " + author1.getLastName() + " " + book1.getName() + " " + book1.getPublishingYear() + "г.");
+    }
+}
