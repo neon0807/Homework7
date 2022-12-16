4c0e66ce40840add383e3ab4f4a01849bf5ee22a
neon0807 <vitaliivorob3@gmail.com>
2022-12-16 15:07:05 +0300

    Проверьте ДЗ

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String name;
     private int publishingYear;
@@ -24,4 +26,25 @@ public class Book {
         this.publishingYear = publishingYear;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", publishingYear=" + publishingYear +
+                ", author=" + author +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishingYear == book.publishingYear && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, publishingYear, author);
+    }
 }
